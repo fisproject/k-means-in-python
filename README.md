78fb6dd529a0354f4587332170f9046dc732a443
ttsy <japan.t.egawa@gmail.com>
2017-04-02 13:11:38 +0900

    Improve kmeans.py

--- README.md
+++ README.md
@@ -2,19 +2,20 @@ k-means-in-python
 ====
 
 ## Overview
-・K-means in python
+K-means in python
 
 ## Description
 
 ## Usage
-```
+```python
 $ python kmeans.py
-Iter :  0  Quantization Error :  601.122433764  Diff :  871.573005395
-Iter :  1  Quantization Error :  327.925733963  Diff :  273.196699801
-Iter :  2  Quantization Error :  270.482767355  Diff :  57.4429666081
-Iter :  3  Quantization Error :  261.264711188  Diff :  9.21805616624
-Iter :  4  Quantization Error :  260.016662684  Diff :  1.24804850477
-Iter :  5  Quantization Error :  260.016662684  Diff :  0.0
+('iter: ', 0, ' quantization Error: ', 779.69608945427467, ' diff: ', 529.62363959615504)
+('iter: ', 1, ' quantization Error: ', 363.07879685667001, ' diff: ', 416.61729259760466)
+('iter: ', 2, ' quantization Error: ', 282.14240512661081, ' diff: ', 80.936391730059199)
+('iter: ', 3, ' quantization Error: ', 264.63561221752815, ' diff: ', 17.506792909082662)
+('iter: ', 4, ' quantization Error: ', 260.59540723329656, ' diff: ', 4.0402049842315932)
+('iter: ', 5, ' quantization Error: ', 260.01666268357656, ' diff: ', 0.57874454971999967)
+('iter: ', 6, ' quantization Error: ', 260.01666268357656, ' diff: ', 0.0)
 ```
 ## Licence
 Licensed under the [MIT](http://opensource.org/licenses/MIT) license.
--- kmeans.py
+++ kmeans.py
@@ -1,125 +1,134 @@
-#coding:utf-8
+#!/bin/python
+# -*- coding: utf-8 -*-
+
 import sys
 import numpy as np
 import sklearn.decomposition as decop
 import matplotlib.pyplot as plt
 
 # 標準化
-def scale(X):
-    col = X.shape[1]
-    mu = np.mean(X, axis=0)
-    sigma = np.std(X, axis=0)
+def scale(data):
+    col = data.shape[1]
+    mu = np.mean(data, axis=0)
+    sigma = np.std(data, axis=0)
 
-    for i in range(col):
-        X[:,i] = (X[:,i] - mu[i]) / sigma[i]
+    for i in xrange(col):
+        data[:,i] = (data[:,i] - mu[i]) / sigma[i]
 
-    return X
+    return data
 
-# 所属クラスタの割り当て : 各piを固定して, eを∂について最小化する
-def D(K, N, r):
-    for n in range(N):
-        i = -1
-        min_ = sys.maxint
+class Kmeans(object):
+    def __init__(self, data, clusters):
+        self.X = data
+        self.N = len(self.X)
+        self.K = clusters
+        self.mean = np.random.rand(self.K, 2)
+        self.r = np.zeros((self.N, self.K))
+        self.r[:, 0] = 1
 
-        for k in range(K):
-            tmp = np.linalg.norm(X[n] - mean[k]) ** 2
-            if tmp < min_:
-                i = k
-                min_ = tmp
+    # 所属クラスタの割り当て : 各piを固定して, eを∂について最小化する
+    def clustering(self):
+        for n in xrange(self.N):
+            i = -1
+            min_ = sys.maxint
 
-        for k in range(K):
-            if k == i:
-                r[n, k] = 1
-            else:
-                r[n, k] = 0
-    return r
+            for k in xrange(self.K):
+                tmp = np.linalg.norm(self.X[n] - self.mean[k]) ** 2
+                if tmp < min_:
+                    i = k
+                    min_ = tmp
 
-# 平均ベクトルの算出 : 各∂iを固定して, eをpについて最小化する
-def P(K, N, mean):
-    for k in range(K):
-        numerator = 0.0
-        denominator = 0.0
-        for n in range(N):
-            numerator += r[n, k] * X[n]
-            denominator += r[n, k]
-        mean[k] = numerator / denominator
+            for k in xrange(self.K):
+                if k == i:
+                    self.r[n, k] = 1
+                else:
+                    self.r[n, k] = 0
+        return self.r
 
-    return mean
+    # 平均ベクトルの算出 : 各∂iを固定して, eをpについて最小化する
+    def mean_vec(self):
+        for k in xrange(self.K):
+            numerator = 0.0
+            denominator = 0.0
+            for n in xrange(self.N):
+                numerator += self.r[n, k] * self.X[n]
+                denominator += self.r[n, k]
+            self.mean[k] = numerator / denominator
 
-# 量子化誤差
-def E(X, mean, r):
-    e = 0.0
-    for n in range(len(X)):
-        e += sum([r[n, k] * np.linalg.norm(X[n]-mean[k]) ** 2 for k in range(K)])
-    return e
+        return self.mean
 
-# 収束判定 : 量子化誤差の変動で収束を判定する
-def check_convergence(p, np):
-    e = p - np
-    if e < 0.01:
-        return (True, e)
-    else:
-        return (False, e)
+    # 量子化誤差
+    def error(self):
+        e = 0.0
+        for n in xrange(self.N):
+            e += sum([self.r[n, k] * np.linalg.norm(self.X[n]-self.mean[k]) ** 2 for k in xrange(self.K)])
+        return e
 
-# Plot Cluster
-def plot_cluster(X, K, N, r):
-    colors = ['g','b','r']
-    for n in range(N):
-        c = [colors[k] for k in range(K) if r[n, k] == 1]
-        plt.scatter(X[n,0], X[n,1], c=c, marker='o')
-    plt.show()
+    # 収束判定 : 量子化誤差の変動で収束を判定する
+    def check_convergence(self, p, np):
+        e = p - np
+        if e < 0.01:
+            return (True, e)
+        else:
+            return (False, e)
 
-# Plot Quantization Error
-def plot_error(err):
-    plt.plot(err)
-    plt.xlabel('Iteration')
-    plt.ylabel('Quantization Error')
-    plt.show()
+    # Plot Cluster
+    def plot_cluster(self):
+        colors = ['g','b','r']
+        for n in xrange(self.N):
+            c = [colors[k] for k in xrange(self.K) if self.r[n, k] == 1]
+            plt.scatter(self.X[n,0], self.X[n,1], c=c, marker='o')
+        plt.show()
 
-if __name__ == "__main__":
+    # Plot Quantization Error
+    def plot_error(self, err):
+        plt.plot(err)
+        plt.xlabel('Iteration')
+        plt.ylabel('Quantization Error')
+        plt.show()
+
+def main():
     # load data
     data = np.loadtxt('data/wine.data', delimiter=',')
     X = scale(data[:,1:14])
-
-    # number of clusters
-    K = 3
 
     # principal component analysis
     pca = decop.PCA(n_components=2)
     pca.fit(X)
     X = pca.transform(X)
 
-    N = len(X)
-    mean = np.random.rand(K, 2)
-    r = np.zeros((N, K))
-    r[:, 0] = 1
+    # number of clusters: 3
+    km = Kmeans(data=X, clusters=3)
 
     # STEP 1: 初期状態の設定
-    proto = E(X, mean, r)
-    t = 0
+    proto = km.error()
+    iteration = 0
     err = []
 
     while True:
         # STEP 2: 所属クラスタの割り当て
-        r = D(K, N, r)
+        r = km.clustering()
 
         # STEP 3: 平均ベクトルの計算
-        mean = P(K, N, mean)
+        mean = km.mean_vec()
 
         # STEP 4: 量子化誤差の計算
-        new_proto = E(X, mean, r)
+        new_proto = km.error()
         err.append(new_proto)
 
         # STEP 5: 収束判定
-        res = check_convergence(proto, new_proto)
+        res = km.check_convergence(proto, new_proto)
+        print('iter: ', iteration, ' quantization Error: ', new_proto, ' diff: ', res[1])
 
-        print 'Iter : ', t, ' Quantization Error : ', new_proto, ' Diff : ', res[1]
-        plot_cluster(X, K, N, r)
+        km.plot_cluster()
 
         if res[0] is False:
             proto = new_proto # 量子化誤差の保持
-            t += 1
+            iteration += 1
         else:
             break
 
-    plot_error(err)
+    km.plot_error(err)
+
+if __name__ == "__main__":
+    main()
